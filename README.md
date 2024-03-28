dfe3d296bd190a5b63fecfc4f969f724427e3713
Brian Masney <bmasney@redhat.com>
2024-03-28 11:44:52 -0400

    initial public release of the early service example
    Signed-off-by: Brian Masney <bmasney@redhat.com>

---
+++ README.md
@@ -0,0 +1,114 @@
+## Early Service Example
+
+When working in some industries with strict boot time requirements, booting
+all the way to the root filesystem is not quick enough to meet the timing
+requirements for some use cases. This project shows how to start a
+short-lived service in the initial ramdisk (initrd), and pass the running
+state to a long-lived service that is started from the root filesystem.
+
+The [example program](early-service.c) has two parts: 1) a timer that
+periodically prints and increments a counter, and 2) a server that listens
+for commands on a UNIX domain socket. This is wired together like so:
+
+- The [early-service-initrd.service](conf/early-service-initrd.service) systemd
+  unit is started inside the initrd and exposes the short-lived server at the
+  UNIX domain socket `/run/early-service-initrd/early-service.sock`. The
+  contents of the `/run` directory in the initrd are also exposed to `/run` on
+  the root filesystem.
+
+- Later when systemd is started from the root filesystem,
+  [early-service.service](conf/early-service.service) is started. This exposes
+  the long-running server at the UNIX domain socket
+  `/run/early-service/early-service.sock`. Separately, it is also told to read
+  the current state of the version running in the initrd from the UNIX domain
+  socket `/run/early-service-initrd/early-service.sock`. Once the state is
+  passed to the version running on the root filesystem, the process that was
+  started from the initrd is told to terminate.
+
+- There is a [dracut module](conf/module-setup.sh) that will tell dracut to
+  include the necessary files in the initrd.
+
+The following annotated log output from `journalctl` shows the two processes
+starting, and exchanging state:
+
+    # early-service-initrd.service is started inside the initrd.
+    Mar 28 10:39:17 localhost early-service[324]: Listening on UNIX socket /run/early-service-initrd/early-service.sock
+    Mar 28 10:39:18 localhost early-service[324]: 0
+    Mar 28 10:39:18 localhost early-service[324]: 1
+    Mar 28 10:39:18 localhost early-service-initrd[324]: ** Message: 10:39:18.223: 2
+    Mar 28 10:39:18 localhost early-service-initrd[324]: ** Message: 10:39:18.323: 3
+    Mar 28 10:39:18 localhost early-service-initrd[324]: ** Message: 10:39:18.424: 4
+    Mar 28 10:39:18 localhost early-service-initrd[324]: ** Message: 10:39:18.524: 5
+    # early-service.service is started from the root filesystem; reads state from running initrd process.
+    Mar 28 10:39:18 localhost early-service[432]: Reading starting position from socket /run/early-service-initrd/early-service.sock
+    Mar 28 10:39:18 localhost early-service-initrd[324]: ** Message: 10:39:18.539: Returning counter to client and terminating the process
+    Mar 28 10:39:18 localhost early-service[432]: Listening on UNIX socket /run/early-service/early-service.sock
+    Mar 28 10:39:18 localhost systemd[1]: early-service-initrd.service: Deactivated successfully.
+    Mar 28 10:39:18 localhost early-service[432]: 6
+    Mar 28 10:39:18 localhost early-service[432]: 7
+    # Only the version started from the root filesystem is now running.
+
+It's meant to be that you will have some minimal service that runs in the
+initrd that does as little as possible, and passes it's state to the fully
+featured services running from the root filesystem. The initrd version should
+only be running for a few seconds at most.
+
+
+## Commands available on UNIX domain socket
+
+The following commands are available over the UNIX domain socket at
+`/run/early-service/early-service.sock`:
+
+- `get_counter`
+- `get_counter_and_terminate`
+- `set_counter ###`
+
+You can test the API by using Netcat:
+
+    $ sudo dnf install nc
+    $ echo "set_counter 100" | sudo nc -U /run/early-service/early-service.sock
+    previous value 502
+    $ echo "get_counter" | sudo nc -U /run/early-service/early-service.sock
+    137
+    $ echo "get_counter" | sudo nc -U /run/early-service/early-service.sock
+    141
+
+
+## Why not start long running services from the initrd?
+
+Here's some reasons why you don't want to have long-running, fully featured
+services started in the initrd:
+
+- You can't leak references to any files on the initrd, otherwise the kernel
+  won't be able to free that memory when the initrd is unmounted.
+
+- The initrd is a cpio archive, and increasing the size of the initrd is going
+  to increase the kernel boot time since it will need to uncompress and extract
+  the cpio archive.
+
+- Any services started from the initrd will be started before the SELinux
+  policy is loaded. Services started from the initrd will run with the
+  `kernel_t` label.
+
+- Services started from the initrd can't depend on almost anything like mounts,
+  devices, services, dbus, etc so it's difficult to develop software of any
+  complexity.
+
+- Adding all of these dependencies to the initrd is only going to move the
+  timing bottlenecks booting from the root filesystem to the initrd.
+
+
+## Integrating this into CentOS Automotive Sample Images
+
+You can use the following commands to build a RPM, and incorporate that
+RPM into an image created by the
+[Automotive Sample Images](https://gitlab.com/CentOS/automotive/sample-images).
+
+    scripts/create-rpm.sh early-service.spec
+    createrepo_c ~/rpmbuild/RPMS/x86_64/
+    cd /path/to/sample-images/osbuild-manifests
+    make DEFINES+='extra_repos=[{"id":"local","baseurl":"file:///home/masneyb/rpmbuild/RPMS/x86_64"}] extra_rpms=["early-service"] image_enabled_services=["early-service"]' cs9-qemu-developer-regular.x86_64.qcow2
+
+If you are incrementally rebuilding the same RPM for testing, then you'll need
+to run `dnf clean all` as your regular user to clean the dnf caches before
+rerunning the `make` command.
---
+++ early-service.c
@@ -0,0 +1,312 @@
+// SPDX-License-Identifier: Apache-2.0
+
+#include <gio/gio.h>
+#include <glib.h>
+#include <glib/gstdio.h>
+#include <stdio.h>
+
+static gint timer_delay_ms = 100;
+static gchar *server_socket_path;
+static gchar *client_socket_path;
+static GMainLoop *loop;
+static gboolean survive_systemd_kill_signal = FALSE;
+
+// Command line arguments
+static GOptionEntry entries[] = {
+	{ "timer_delay_ms", 'd', 0, G_OPTION_ARG_INT, &timer_delay_ms,
+	  "Timer delay in milliseconds", NULL, },
+	{ "server_socket_path", 's', 0, G_OPTION_ARG_FILENAME,
+	  &server_socket_path, "Server UNIX domain socket path to listen on",
+	  NULL, },
+	{ "client_socket_path", 'c', 0, G_OPTION_ARG_FILENAME,
+	  &client_socket_path, "UNIX domain socket path to read current state",
+	  NULL, },
+	{ "survive_systemd_kill_signal", 0, 0, G_OPTION_ARG_NONE,
+	  &survive_systemd_kill_signal,
+	  "Set argv[0][0] to '@' when running in initrd", NULL },
+	{ NULL }
+};
+
+struct counter_data {
+	int counter;
+};
+
+struct connection_info {
+	GSocketConnection *connection;
+	struct counter_data *cntr;
+	char buf[50];
+	gboolean terminate_at_end;
+};
+
+static gboolean timer_callback(gpointer data)
+{
+	struct counter_data *cntr = data;
+
+	g_message("%d", cntr->counter++);
+
+	return G_SOURCE_CONTINUE;
+}
+
+/*
+ * The next block of functions are for the server that's exposed on a UNIX
+ * domain socket. This is all done with asynchronous IO so that nothing will
+ * block the glib main loop.
+ */
+
+void server_free_connection(struct connection_info *conn)
+{
+	gboolean terminate = conn->terminate_at_end;
+
+	g_object_unref(G_SOCKET_CONNECTION(conn->connection));
+	g_free(conn);
+
+	if (terminate)
+		g_main_loop_quit(loop);
+}
+
+void server_message_sent(GObject *source_object, GAsyncResult *res,
+			 gpointer user_data)
+{
+	server_free_connection(user_data);
+}
+
+void server_send_message(struct connection_info *conn)
+{
+	g_output_stream_write_async(g_io_stream_get_output_stream(G_IO_STREAM(conn->connection)),
+				    conn->buf, strlen(conn->buf),
+				    G_PRIORITY_DEFAULT, NULL,
+				    server_message_sent, conn);
+}
+
+#define SERVER_SET_COUNTER_COMMAND "set_counter "
+
+void server_message_ready(GObject *source_object, GAsyncResult *res,
+			  gpointer user_data)
+{
+	GInputStream *istream = G_INPUT_STREAM(source_object);
+	struct connection_info *conn = user_data;
+	GError *error = NULL;
+	int new_counter;
+	char *pos;
+
+	g_input_stream_read_finish(istream, res, &error);
+	if (error != NULL) {
+		g_printerr("%s", error->message);
+		g_error_free(error);
+		return;
+	}
+
+	if ((pos = strchr(conn->buf, '\n')) != NULL)
+		*pos = '\0';
+
+	if (g_str_equal(conn->buf, "get_counter")) {
+		g_message("Returning counter to client");
+
+		g_snprintf(conn->buf, sizeof(conn->buf),
+			   "%d\n", conn->cntr->counter);
+		server_send_message(conn);
+	} else if (g_str_equal(conn->buf, "get_counter_and_terminate")) {
+		g_message("Returning counter to client and terminating the process");
+
+		conn->terminate_at_end = TRUE;
+		g_snprintf(conn->buf, sizeof(conn->buf),
+			   "%d\n", conn->cntr->counter);
+		server_send_message(conn);
+	} else if (g_str_has_prefix(conn->buf, SERVER_SET_COUNTER_COMMAND)) {
+		new_counter = g_ascii_strtoll(conn->buf + sizeof(SERVER_SET_COUNTER_COMMAND) - 1,
+					      NULL, 10);
+
+		g_message("Setting the counter to %d", new_counter);
+
+		g_snprintf(conn->buf, sizeof(conn->buf),
+			   "previous value %d\n", conn->cntr->counter);
+		conn->cntr->counter = new_counter;
+		server_send_message(conn);
+	} else {
+		g_message("Unknown message '%s' from client", conn->buf);
+		server_free_connection(conn);
+	}
+}
+
+static gboolean server_incoming_connection(GSocketService *service,
+					   GSocketConnection *connection,
+					   GObject *source_object,
+					   gpointer user_data)
+{
+	struct connection_info *conn = g_new0(struct connection_info, 1);
+
+	conn->connection = g_object_ref(connection);
+	conn->cntr = user_data;
+
+	g_input_stream_read_async(g_io_stream_get_input_stream(G_IO_STREAM(connection)),
+				  conn->buf, sizeof(conn->buf),
+				  G_PRIORITY_DEFAULT, NULL,
+				  server_message_ready, conn);
+
+	return FALSE;
+}
+
+static GSocketService *create_unix_domain_server(char *server_socket_path,
+						 struct counter_data *cntr)
+{
+	GSocketService *service;
+	GSocketAddress *address;
+	GError *error = NULL;
+
+	service = g_socket_service_new();
+	if (service == NULL) {
+		g_printerr("Error creating socket service.\n");
+		return NULL;
+	}
+
+	address = g_unix_socket_address_new(server_socket_path);
+	if (address == NULL) {
+		g_printerr("Error creating socket address.\n");
+		g_object_unref(service);
+		return NULL;
+	}
+
+	if (!g_socket_listener_add_address(G_SOCKET_LISTENER(service),
+					   G_SOCKET_ADDRESS(address),
+					   G_SOCKET_TYPE_STREAM,
+					   G_SOCKET_PROTOCOL_DEFAULT,
+					   NULL, NULL, &error)) {
+		g_printerr("Error binding socket: %s\n", error->message);
+		g_error_free(error);
+		g_object_unref(service);
+		g_object_unref(address);
+		return NULL;
+	}
+
+	g_signal_connect(service, "incoming",
+			 G_CALLBACK(server_incoming_connection), cntr);
+
+	g_socket_service_start(service);
+
+	return service;
+}
+
+/*
+ * This is the client that reads the current state from another process
+ * via a UNIX domain socket. This is done using synchronous IO since this
+ * is only called on boot up and we will be blocked waiting to read the
+ * current state.
+ */
+
+#define CLIENT_GET_COUNTER_COMMAND "get_counter_and_terminate\n"
+
+int read_counter_from_server(gchar *server_path)
+{
+	GSocketConnection *connection;
+	GSocketAddress *address;
+	GSocketClient *client;
+	GError *error = NULL;
+	gssize bytes_read;
+	gchar buf[100];
+
+	client = g_socket_client_new();
+	address = g_unix_socket_address_new(server_path);
+
+	connection = g_socket_client_connect(client,
+					     G_SOCKET_CONNECTABLE(address),
+					     NULL, &error);
+	if (error != NULL) {
+		/*
+		 * We shouldn't terminate when we can't read the current state.
+		 * Just start over.
+		 */
+		g_printerr("Error connecting to socket: %s", error->message);
+		g_error_free(error);
+		return 0;
+	}
+
+	GInputStream *input_stream = g_io_stream_get_input_stream(G_IO_STREAM(connection));
+	GOutputStream *output_stream = g_io_stream_get_output_stream(G_IO_STREAM(connection));
+
+	g_output_stream_write(output_stream, CLIENT_GET_COUNTER_COMMAND,
+			      strlen(CLIENT_GET_COUNTER_COMMAND), NULL, &error);
+	if (error != NULL) {
+		g_printerr("Error writing to socket: %s", error->message);
+		g_error_free(error);
+		return 0;
+	}
+
+	bytes_read = g_input_stream_read(input_stream, buf, sizeof(buf) - 1,
+					 NULL, &error);
+	if (error != NULL) {
+		g_printerr("Error reading from socket: %s", error->message);
+		g_error_free(error);
+		return 0;
+	}
+
+	buf[bytes_read] = '\0';
+
+	g_object_unref(connection);
+	g_object_unref(client);
+
+	return g_ascii_strtoll(buf, NULL, 10);
+}
+
+int get_initial_counter(void)
+{
+	if (client_socket_path == NULL)
+		return 0;
+
+	g_message("Reading starting position from socket %s",
+		  client_socket_path);
+
+	return read_counter_from_server(client_socket_path);
+}
+
+int main(int argc, char **argv)
+{
+	GSocketService *service = NULL;
+	GOptionContext *context;
+	GError *error = NULL;
+
+	context = g_option_context_new("- Example Early Service");
+	g_option_context_add_main_entries(context, entries, NULL);
+	if (!g_option_context_parse(context, &argc, &argv, &error)) {
+		g_printerr("option parsing failed: %s\n", error->message);
+		return 1;
+	}
+
+	if (survive_systemd_kill_signal) {
+		/*
+		 * See https://systemd.io/ROOT_STORAGE_DAEMONS/ for more details
+		 * about having this started inside the initrd running when the
+		 * system transitions to running services from the root
+		 * filesystem. systemd v255 and higher has the option
+		 * SurviveFinalKillSignal=yes that can be used instead.
+		 */
+		argv[0][0] = '@';
+	}
+
+	loop = g_main_loop_new(NULL, FALSE);
+
+	struct counter_data cntr = {
+		.counter = get_initial_counter()
+	};
+
+	guint timer_id = g_timeout_add(timer_delay_ms, timer_callback, &cntr);
+
+	if (server_socket_path != NULL) {
+		g_message("Listening on UNIX socket %s", server_socket_path);
+		service = create_unix_domain_server(server_socket_path, &cntr);
+		if (service == NULL)
+			return 1;
+	} else
+		g_message("Not listening on a UNIX socket.");
+
+	g_main_loop_run(loop);
+
+	g_source_remove(timer_id);
+	if (service != NULL) {
+		g_socket_service_stop(service);
+		g_unlink(server_socket_path);
+	}
+
+	g_main_loop_unref(loop);
+
+	return 0;
+}
